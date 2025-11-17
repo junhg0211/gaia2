148bd30a4c46bc9701e115e7b5b84eb589b5d1a9
Sch <junhg0211@gmail.com>
2025-11-17 14:57:28 +0900

    Update WebSocket server URL to include dynamic port number

--- server/index.js
+++ server/index.js
@@ -62,4 +62,4 @@ wss.on('connection', (ws) => {
   });
 });
 
-console.log('WebSocket server is running on ws://localhost:8080');
+console.log(`WebSocket server is running on ws://localhost:${port}`);
