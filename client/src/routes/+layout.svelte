bf3cf1b476e0b859049f6f4c2494651131d9c66a
BackGwa <backgwa@icloud.com>
2025-11-17 14:40:27 +0900

    Add custom font 'unifont' for the layout

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -7,12 +7,20 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
   <style>
+    @font-face {
+      font-family: 'unifont';
+      src: url("/fonts/unifont-17.0.03.otf") format('truetype');
+      font-weight: normal;
+      font-style: normal;
+    }
+
     body, html {
       margin: 0;
       padding: 0;
       overflow: hidden;
       background-color: #2c2c2c;
       color: white;
+      font-family: 'unifont', monospace;
     }
   </style>
 </svelte:head>
